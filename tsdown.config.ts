b7933853b78501aeb7bb104a0d2a3ea380f2a1d9
Roman <dev@rman.dev>
2026-09-05 16:39:05 +0100

    chore(deps): update tsdown (#108)
    * chore(deps): update tsdown
    
    * Update tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: { index: "src/index.ts", schemas: "src/schemas.ts" },
   exports: true,
-  dts: { tsgo: true },
+  dts: true,
   inputOptions: { experimental: { attachDebugInfo: "none" } },
   outputOptions: { minifyInternalExports: false },
 });
